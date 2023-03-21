673ce3a6a1826477017b07fc4678dd9eed463e5f
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-03-21 19:33:57 +0100

    First part of login form

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,9 +1,26 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from '@builder.io/qwik';
+import { Form } from '@builder.io/qwik-city';
+import Input from '~/components/shared/input/Input';
+import './LoginForm.scss';
 
 export default component$(() => {
-  return(
-    <div>
-      
+  const username = useSignal('hello');
+
+  return (
+    <div class='login-form'>
+      <h2>Login</h2>
+      <Form>
+        <Input/>
+        <input
+          type='text'
+          value={username.value}
+          onInput$={(event) => {
+            username.value = (event.target as any).value;
+          }}
+        />
+        {/* <input type='text' bind:value={username} /> */}
+        <button type='submit'>Login</button>
+      </Form>
     </div>
-  )
-})
+  );
+});
