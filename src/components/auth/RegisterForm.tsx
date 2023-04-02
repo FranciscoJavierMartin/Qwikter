5fd307e92d4c2e9326131608050a6ac738fa5573
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-04-02 14:01:44 +0200

    Register in server

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -24,44 +24,46 @@ const registrationFormValidation = z
   });
 
 export const useRegister = globalAction$(
-  async ({ username, password, email, confirmPassword }, { fail }) => {
-    // let res: FailReturn<{ message: string }> | RegisterResponse;
-    // if (password !== confirmPassword) {
-    //   res = fail(400, { message: "Passwords don't match" });
-    // } else {
-    //   try {
-    //     const response = await fetch('http://localhost:5173/api/login', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify({
-    //         username,
-    //         password,
-    //       }),
-    //     });
-    //     const responseBody = await response.json();
-    //     if (responseBody.error) {
-    //       res = fail(responseBody.statusCode, {
-    //         message: responseBody.message,
-    //       });
-    //     } else {
-    //       res = responseBody;
-    //     }
-    //   } catch (e) {
-    //     res = fail(400, { message: 'Something goes wrong' });
-    //   }
-    // }
-    // return res;
+  async ({ username, password, email }, { fail }) => {
+    let res: FailReturn<{ message: string }> | RegisterResponse;
+
+    try {
+      const response = await fetch('http://localhost:5173/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+          avatarColor: 'blue',
+        }),
+      });
+
+      const responseBody = await response.json();
+
+      if (responseBody.error) {
+        res = fail(responseBody.statusCode, {
+          message: responseBody.message,
+        });
+      } else {
+        res = responseBody;
+      }
+    } catch (e) {
+      res = fail(400, { message: 'Something goes wrong' });
+    }
+
+    return res;
   },
   zod$(registrationFormValidation)
 );
 
 export default component$(() => {
-  const username = useSignal<string>('');
-  const email = useSignal<string>('');
-  const password = useSignal<string>('');
-  const confirmPassword = useSignal<string>('');
+  const username = useSignal<string>('Test');
+  const email = useSignal<string>('test@test.com');
+  const password = useSignal<string>('test');
+  const confirmPassword = useSignal<string>('test');
   const keepLogging = useSignal<boolean>(false);
   const register = useRegister();
 
@@ -73,7 +75,12 @@ export default component$(() => {
           {register.value?.formErrors?.join(',')}
         </div>
       )}
-      <Form action={register}>
+      <Form
+        action={register}
+        onSubmitCompleted$={(e) => {
+          console.log('Success', e.detail.value as RegisterResponse);
+        }}
+      >
         <Input
           id='username'
           name='username'
---
+++ src/routes/api/register/index.ts
@@ -0,0 +1,17 @@
+import type { RequestHandler } from '@builder.io/qwik-city';
+
+export const onPost: RequestHandler = async ({ json, parseBody }) => {
+  const body = await parseBody();
+
+  const response = await fetch('http://localhost:3020/api/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+
+  json(response.status, {
+    ...(await response.json()),
+  });
+};
